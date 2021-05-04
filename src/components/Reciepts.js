3e1721484308282be553da0a739da83f1f3e3a68
obidike franklin <frankobidike20@gmail.com>
2021-05-03 22:51:10 -0800

    build

--- src/components/Reciepts.js
+++ src/components/Reciepts.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function Reciepts() {
     const removeLS = () => {
+        window.print()
         localStorage.removeItem('power')
         window.location.reload()
     }
@@ -14,12 +15,12 @@ function Reciepts() {
                         Electrik<small>.ng</small>
                     </div>
                     <h2 className="card-title mb-3">Payment Successful</h2>
-                    <label className="mb-2">Address:</label>  {detail.address}<br />
-                    <label className="mb-2">Amount:</label>  {detail.amount}<br />
+                    <label className="mb-2">Address:</label>  {detail?.address}<br />
+                    <label className="mb-2">Amount:</label>  {detail?.amount}<br />
                     <label className=" mb-2">Units Purchased:</label>  {detail.units} <br />
                     <label className=" mb-2">Meter Token:</label> {detail.meter_token} <br />
                     <label className=" mb-4">Reference No:</label> {detail.ref} <br />
-                    <div onClick={removeLS} className="btn w-50 mt-3 btn-primary">Done</div>
+                    <div onClick={removeLS} className="btn w-50 mt-3 btn-primary">Print</div>
                 </div>
             </div>
         </div>
